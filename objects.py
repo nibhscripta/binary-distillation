3ffdd36d9b15117843c453f65e86eac55da6717e
mkl <nibhscripta@tutanota.com>
2023-04-25 11:30:20 -0700

    Overhaul the process for evaluation of stages

--- objects.py
+++ objects.py
@@ -1,68 +1,176 @@
-import scipy, dataclasses, numpy, matplotlib, warnings, typing, pydantic
+import scipy, dataclasses, numpy, matplotlib, warnings, typing
 
-@dataclasses.dataclass 
-class VaporLiquidEquilibriumLine():
+
+
+def _evaluate_minimum_reflux_ratio(eq, x_F, x_D, q):
+    '''
+    Finds the minimum reflux ratio of a binary distillation column by finding the point at which the feed and enriching line intersect the equilibrium curve.
+
+    equilibrium_line: Set of x/y data that describes relationship between the composition of a binary mixture in the liquid and vapor phases.
+
+    x_F: Feed composition
+
+    x_D: Distillate composition
+
+    q: Number which describes the slope of the feed line.
+    '''
+    if q == 1:
+        xp = x_F
+    else:
+        # feed line
+        f_F = lambda x: q / (q - 1) * x - x_F / (q - 1)
+
+        # intersection between feed and equilibrium lines; evaluates to 0 at x'
+        f_p = lambda x: numpy.interp(x, eq.x, eq.y) - f_F(x)
+
+        # x'
+        xp = scipy.optimize.fsolve(f_p, x_F)[0]
+    
+    # y'
+    yp = numpy.interp(xp, eq.x, eq.y)
+
+    # minimum reflux ration
+    return (x_D - yp) / (yp - xp)
+
+
+
+@dataclasses.dataclass
+class OperatingLine():
     x: numpy.ndarray
     y: numpy.ndarray
+    f: typing.Optional[typing.Callable]
+    xpp: float
+    ypp: float
 
-@dataclasses.dataclass 
-class BinaryDistillatioOperatingLine(VaporLiquidEquilibriumLine):
+
+
+def _operating_lines_specified_reflux(x_F, x_D, x_W, q, R):
+    '''
+    Creates a enriching and stripping lines for a specified reflux ratio.
+
+    x_F: Feed composition
+
+    x_D: Distillate composition
+
+    x_W: Worm/bottom composition
+
+    q: Number which describes the slope of the feed line.
+
+    R: reflux ratio
+    '''
+    # enriching line
+    f_e = lambda x: R / (R + 1) * x + x_D / (R + 1)
+
+    if q == 1:
+        xpp = x_F
+    else:
+        # feed line
+        f_F = lambda x: q / (q - 1) * x - x_F / (q - 1)
+
+        # x''
+        xpp = scipy.optimize.fsolve(lambda x: f_F(x) - f_e(x), x_F)[0]
+
+    # y''
+    ypp = f_e(xpp)
+
+    x_e = numpy.array([xpp, x_D])
+    y_e = f_e(x_e)
+
+    # stripping line
+    f_s = lambda x: (y_e[0] - x_W) / (x_e[0] - x_W) * (x - x_W) + x_W
+    x_s = numpy.array([x_W, xpp])
+    y_s = f_s(x_s)
+
+    return OperatingLine(x_s, y_s, f_s, xpp, ypp), OperatingLine(x_e, y_e, f_e, xpp, ypp)
+
+
+
+def _operating_lines_specified_boilup(x_F, x_D, x_W, q, B):
+    '''
+    Creates a enriching and stripping lines for a specified boilup ratio.
+
+    x_F: Feed composition
+
+    x_D: Distillate composition
+
+    x_W: Worm/bottom composition
+
+    q: Number which describes the slope of the feed line.
+
+    B: Boilup ratio
+    '''
+    # stripping line
+    f_s = lambda x: (1 + 1 / B) * x - x_W / B
+
+    if q == 1:
+        xpp = x_F
+    else:
+        # feed line
+        f_F = lambda x: q / (q - 1) * x - x_F / (q - 1)
+
+        # x''
+        xpp = scipy.optimize.fsolve(lambda x: f_F(x) - f_s(x), x_F)[0]
+
+    # y''
+    ypp = f_s(xpp)
+
+    x_s = numpy.array([x_W, xpp])
+    y_s = f_s(x_s)
+
+    # enriching line
+    f_e = lambda x: (x_D - y_s[-1]) / (x_D - xpp) * (x - x_D) + x_D
+    x_e = numpy.array([xpp, x_D])
+    y_e = f_e(x_e)
+
+    return OperatingLine(x_s, y_s, f_s, xpp, ypp), OperatingLine(x_e, y_e, f_e, xpp, ypp)
+
+
+
+@dataclasses.dataclass
+class BinaryVaporLiquidEquilibriumLine():
+    x: numpy.ndarray = dataclasses.field(init=False)
+    y: numpy.ndarray = dataclasses.field(init=False)
+    f: typing.Optional[typing.Callable] = dataclasses.field(init=False)
+
+    def __init__(self, *args):
+        if len(args) == 1:
+            self.f = args[0]
+            self.x = numpy.linspace(0, 1, 1000)
+            self.y = self.f(self.x)
+        elif len(args) == 2:
+            self.x = args[0]
+            self.y = args[1]
+
+
+
+@dataclasses.dataclass
+class BinaryDistillationOperatingLine():
     x_F: float
     x_D: float
     x_W: float
-
-@dataclasses.dataclass 
-class McCabeThieleAnalysis():
-    x: numpy.ndarray
-    y: numpy.ndarray
-    N: float
-    eq: VaporLiquidEquilibriumLine
-    op: BinaryDistillatioOperatingLine
-
-    def plot(self):
-        plot = matplotlib.pyplot.plot
-        plot(self.eq.x, self.eq.y, label="Equilibrium Line")
-        plot(self.op.x, self.op.y, label="Operating Line")
-        plot([self.op.x_F, self.op.x[999]], [self.op.x_F, self.op.y[999]], label="Feed Line")
-        plot(self.eq.x, self.eq.x, "tab:grey", linestyle="--")
-        plot(self.x, self.y, "k", label="Stages")
-        matplotlib.pyplot.xlabel("Liquid Composition")
-        matplotlib.pyplot.ylabel("Vapor Composition")
-
-    def __str__(self):
-        return f"Column with {self.N} theoretical stages."
-
-@pydantic.dataclasses.dataclass 
-class AntoineParameters():
-    A: float
-    B: float
-    C: float
-    log_type: typing.Literal["ln", "log"]
-
-    def P(self, T):
-        if self.log_type == "ln":
-            return numpy.exp(self.A - self.B / (T + self.C))
-        elif self.log_type == "log":
-            return 10**(self.A - self.B / (T + self.C))
-        
-@dataclasses.dataclass 
-class RaoultsLawVaporLiquidEquilibrium():
-    P_A: AntoineParameters
-    P_B: AntoineParameters
-    P: float
-    Line: VaporLiquidEquilibriumLine = dataclasses.field(init=False)
-
-    def __str__(self):
-        return f"Equilibrium at {self.P}. P_A: {self.P_A} P_B: {self.P_B}"
+    equilibrium: BinaryVaporLiquidEquilibriumLine
+    q: float
+    R: float = None
+    B: float = None
+    R_min: float = dataclasses.field(init=False)
+    e: OperatingLine = dataclasses.field(init=False)
+    s: OperatingLine = dataclasses.field(init=False)
+    x: numpy.ndarray = dataclasses.field(init=False)
+    y: numpy.ndarray = dataclasses.field(init=False) 
 
     def __post_init__(self):
-        x = numpy.linspace(0, 1, 1000)
-        y = numpy.zeros(x.shape[0])
-        P = self.P
-        for i in range(x.shape[0]):
-            x_i = x[i]
-            P_T = lambda T: x_i * self.P_A.P(T) + (1 - x_i) * self.P_B.P(T)
-            T = scipy.optimize.fsolve(lambda T: P - P_T(T), 298)[0]
-            y[i] = x_i * self.P_A.P(T) / P 
-        
-        self.Line = VaporLiquidEquilibriumLine(x, y)
+        self.R_min = _evaluate_minimum_reflux_ratio(self.equilibrium, self.x_F, self.x_D, self.q)
+
+        if self.R is None and self.B is None:
+            self.R = 1.3 * self.R_min
+            self.s, self.e = _operating_lines_specified_reflux(self.x_F, self.x_D, self.x_W, self.q, self.R)
+        elif self.R is not None:
+            self.s, self.e = _operating_lines_specified_reflux(self.x_F, self.x_D, self.x_W, self.q, self.R)
+        elif self.B is not None:
+            self.s, self.e = _operating_lines_specified_boilup(self.x_F, self.x_D, self.x_W, self.q, self.B)
+
+        self.x = numpy.array([*self.s.x, *self.e.x])
+        self.y = numpy.array([*self.s.y, *self.e.y])
+
+    def __str__(self):
+        return f"x_F: {self.x_F}, x_D: {self.x_D}, x_W: {self.x_W}, q: {self.q}, R: {self.R}, B: {self.B}, R_min: {self.R_min}"
--- operations.py
+++ operations.py
@@ -1,195 +1,26 @@
 import scipy, dataclasses, numpy, matplotlib, warnings
 
-from objects import VaporLiquidEquilibriumLine, McCabeThieleAnalysis, BinaryDistillatioOperatingLine
+def plot_mccabe_thiele_analysis(eq, op, stages):
+    matplotlib.pyplot.plot(eq.x, eq.y, label="Equilibrium Line")
+    matplotlib.pyplot.plot(op.x, op.y, label="Operating Line")
+    matplotlib.pyplot.plot([op.x_F, op.e.xpp,], [op.x_F, op.e.ypp], label="Feed Line")
+    matplotlib.pyplot.plot(stages.x, stages.y, "k", label="Stages")
+    matplotlib.pyplot.plot(eq.x, eq.x, "tab:grey", linestyle="--")
 
-def evaluate_equilibrium_line(equilibrium_relationshiop):
-    '''
-    Create an equilibrium line from an arbitray funcntion that describes relationship between the composition of a binary mixture in the liquid and vapor phases. 
-
-    equilibrium_relationshiop: y=f(x)
-    '''
-    try:
-        equilibrium_relationshiop(0)
-    except:
-        raise TypeError("Argument equilibrium_relationship must take one positional argument only.")
-    x = numpy.linspace(0, 1, 1000)
-
-    return VaporLiquidEquilibriumLine(x, equilibrium_relationshiop(x))
+    matplotlib.pyplot.xlabel("Liquid Composition")
+    matplotlib.pyplot.ylabel("Vapor Composition")
 
 
 
-def _evaluate_minimum_reflux_ratio(equilibrium_line: VaporLiquidEquilibriumLine, x_F, x_D, q):
-    '''
-    Finds the minimum reflux ratio of a binary distillation column by finding the point at which the feed and enriching line intersect the equilibrium curve.
-
-    equilibrium_line: Set of x/y data that describes relationship between the composition of a binary mixture in the liquid and vapor phases.
-
-    x_F: Feed composition
-
-    x_D: Distillate composition
-
-    q: Number which describes the slope of the feed line.
-    '''
-    if q == 1:
-        xp = x_F
-    else:
-        # feed line
-        f_F = lambda x: q / (q - 1) * x - x_F / (q - 1)
-
-        # intersection between feed and equilibrium lines; evaluates to 0 at x'
-        f_p = lambda x: numpy.interp(x, equilibrium_line.x, equilibrium_line.y) - f_F(x)
-
-        # x'
-        xp = scipy.optimize.fsolve(f_p, x_F)[0]
-    
-    # y'
-    yp = numpy.interp(xp, equilibrium_line.x, equilibrium_line.y)
-
-    # minimum reflux ration
-    return (x_D - yp) / (yp - xp)
+@dataclasses.dataclass 
+class BinaryStageAnalysis():
+    x: float
+    y: float
+    N: float
 
 
 
-def _operating_lines_specified_reflux(x_F, x_D, x_W, q, R):
-    '''
-    Creates a enriching and stripping lines for a specified reflux ratio.
-
-    x_F: Feed composition
-
-    x_D: Distillate composition
-
-    x_W: Worm/bottom composition
-
-    q: Number which describes the slope of the feed line.
-
-    R: reflux ratio
-    '''
-    # enriching line
-    f_e = lambda x: R / (R + 1) * x + x_D / (R + 1)
-
-    if q == 1:
-        xpp = x_F
-    else:
-        # feed line
-        f_F = lambda x: q / (q - 1) * x - x_F / (q - 1)
-
-        # x''
-        xpp = scipy.optimize.fsolve(lambda x: f_F(x) - f_e(x), x_F)[0]
-
-    # y''
-    ypp = f_e(xpp)
-
-    x_e = numpy.linspace(xpp, x_D, 1000)
-    y_e = f_e(x_e)
-    # enriching line array
-    e = VaporLiquidEquilibriumLine(x_e, y_e)
-
-    # stripping line
-    f_s = lambda x: (e.y[0] - x_W) / (e.x[0] - x_W) * (x - x_W) + x_W
-    x_s = numpy.linspace(x_W, xpp, 1000)
-    y_s = f_s(x_s)
-    # stripping line array
-    s = VaporLiquidEquilibriumLine(x_s, y_s)
-
-    return s, e, xpp, ypp
-
-
-
-def _operating_lines_specified_boilup(x_F, x_D, x_W, q, B):
-    '''
-    Creates a enriching and stripping lines for a specified boilup ratio.
-
-    x_F: Feed composition
-
-    x_D: Distillate composition
-
-    x_W: Worm/bottom composition
-
-    q: Number which describes the slope of the feed line.
-
-    B: Boilup ratio
-    '''
-    # stripping line
-    f_s = lambda x: (1 + 1 / B) * x - x_W / B
-
-    if q == 1:
-        xpp = x_F
-    else:
-        # feed line
-        f_F = lambda x: q / (q - 1) * x - x_F / (q - 1)
-
-        # x''
-        xpp = scipy.optimize.fsolve(lambda x: f_F(x) - f_s(x), x_F)[0]
-
-    # y''
-    ypp = f_s(xpp)
-
-    x_s = numpy.linspace(x_W, xpp, 1000)
-    y_s = f_s(x_s)
-    # stripping line array
-    s = VaporLiquidEquilibriumLine(x_s, y_s)
-
-    # enriching line
-    f_e = lambda x: (x_D - s.y[-1]) / (x_D - xpp) * (x - x_D) + x_D
-    x_e = numpy.linspace(xpp, x_D, 1000)
-    y_e = f_e(x_e)
-    # enriching line array
-    e = VaporLiquidEquilibriumLine(x_e, y_e)
-
-    return s, e, xpp, ypp
-
-
-
-def binary_distillation_operating_line(x_F, x_D, x_W, q, equilibrium_line: VaporLiquidEquilibriumLine, R=None, B=None):
-    '''
-    Creates a unified operating line spanning from the worm composition to the distillate composition.
-
-    x_F: Feed composition
-
-    x_D: Distillate composition
-
-    x_W: Worm/bottom composition
-
-    q: Number which describes the slope of the feed line.
-
-    equilibrium_line: Set of x/y data that describes relationship between the composition of a binary mixture in the liquid and vapor phases.
-
-    R (Optional): Reflux ratio
-
-    B (Optional): Boilup ratio
-    '''
-
-    # If no reflux or boilup ratio is specified, find the mimimum reflux ration and use 1.3 times the minimum for R.
-    if R is None and B is None:
-        warnings.warn("No reflux or boilup ratio specified. Using 1.3 times R_min.")
-
-        R = 1.3 * _evaluate_minimum_reflux_ratio(equilibrium_line, x_F, x_D, q)       
-    
-    # reflux or boilup ratio cannot both be specified
-    if R is not None and B is not None:
-        raise Warning("System is overspecified. Specify either reflux or boilup ratio, not both.")
-    
-    # for specified reflux ratio
-    if R is not None:
-        s, e, xpp, ypp = _operating_lines_specified_reflux(x_F, x_D, x_W, q, R)
-
-    # for specified boilup ratio
-    elif B is not None:
-        s, e, xpp, ypp = _operating_lines_specified_boilup(x_F, x_D, x_W, q, B)
-
-    # operating line cannot extend past equilivrium curve, violating the 1st law of thermodynamics
-    if ypp > numpy.interp(xpp, equilibrium_line.x, equilibrium_line.y):
-        warnings.warn("Process is infeasible. Operating line extends above equilibrium line.")
-
-    # pack operating lines
-    x = numpy.array([*s.x, *e.x])
-    y = numpy.array([*s.y, *e.y])
-    op = BinaryDistillatioOperatingLine(x, y, x_F, x_D, x_W)
-
-    return op
-    
-
-def step_off_stages(operating_line: VaporLiquidEquilibriumLine, equilibrium_line: VaporLiquidEquilibriumLine, E:float = 1):
+def step_off_top(op, eq, E:float = 1):
     '''
     Perform a McCabe Thiele analysis by stepping down to the distillate composition to the worm composition.
 
@@ -203,21 +34,21 @@ def step_off_stages(operating_line: VaporLiquidEquilibriumLine, equilibrium_line
         raise ValueError("Efficiency must be between 0 and 1 or 1.")
     
     # x values at each stage
-    x = [operating_line.x[-1]]
+    x = [op.x[-1]]
     # y values at each stage
-    y = [operating_line.x[-1]]
+    y = [op.x[-1]]
 
     for i in range(1000):
         y.append(y[2*i])
-        x_eq = numpy.interp(y[2*i], equilibrium_line.y, equilibrium_line.x)
+        x_eq = numpy.interp(y[2*i], eq.y, eq.x)
         x.append(x[2*i-1] - (x[2*i-1] - x_eq) * E)
         x.append(x[2*i+1])
-        y.append(numpy.interp(x[2*i+1], operating_line.x, operating_line.y))
-        if x[2*i+1] < operating_line.x[0]:
-            partial_stage = (x[2*i] - operating_line.x[0]) / (x[2*i]- x[2*i+1])
-            x[2*i+2] = operating_line.x[0]
-            y[2*i+2] = operating_line.x[0]
-            x[2*i+1] = operating_line.x[0]
+        y.append(numpy.interp(x[2*i+1], op.x, op.y))
+        if x[2*i+1] < op.x[0]:
+            partial_stage = (x[2*i] - op.x[0]) / (x[2*i]- x[2*i+1])
+            x[2*i+2] = op.x[0]
+            y[2*i+2] = op.x[0]
+            x[2*i+1] = op.x[0]
             y[2*i+1] = y[2*i]
             N = i + partial_stage 
             break
@@ -225,5 +56,5 @@ def step_off_stages(operating_line: VaporLiquidEquilibriumLine, equilibrium_line
         if i >= 999:
             N = i+1
             warnings.warn(f"Cannot get around pinch point.")
-    
-    return McCabeThieleAnalysis(x, y, N, equilibrium_line, operating_line)
+
+    return BinaryStageAnalysis(x, y, N)
