e42236386d50103b98be72b5a74dbb173109527c
mkl <nibhscripta@tutanota.com>
2023-05-13 11:38:07 -0500

    Fix imports in  __init__.py

--- pystill/__init__.py
+++ pystill/__init__.py
@@ -1,2 +1,2 @@
-from .equilibrium import EquilibriumLine, AntoineEquation, NRTL
-from .design import DistillationColumn
+from pystill.equilibrium import EquilibriumLine, AntoineEquation, NRTL
+from pystill.design import DistillationColumn
